5bf9faf86e6f5b073e464e4316719967391bbc3c
DAN FAGUNDES <danielfagundes589@gmail.com>
2025-08-26 21:23:25 -0300

    Fix: Sidebar
    - Adicionado icones.

--- Frontend/AIssist/src/app/components/sidebar/sidebar.component.ts
+++ Frontend/AIssist/src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    MatIconModule
+  ],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
