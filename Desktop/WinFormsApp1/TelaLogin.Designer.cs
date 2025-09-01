1a300fb40a444862628e49a0945e3252d0cb0092
Joel rodrigues <jrrodrigues108@gmail.com>
2025-09-01 10:37:02 -0300

    Inicio desktop

---
+++ Desktop/WinFormsApp1/TelaLogin.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WinFormsApp1
+{
+    partial class TelaLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            txtlogin = new TextBox();
+            txtPassword = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(549, 357);
+            button1.Name = "button1";
+            button1.Size = new Size(277, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Log in";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // txtlogin
+            // 
+            txtlogin.Location = new Point(549, 228);
+            txtlogin.Name = "txtlogin";
+            txtlogin.Size = new Size(277, 23);
+            txtlogin.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(549, 292);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(277, 23);
+            txtPassword.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(549, 210);
+            label1.Name = "label1";
+            label1.Size = new Size(37, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Login";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(549, 274);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Password";
+            // 
+            // TelaLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1355, 802);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtPassword);
+            Controls.Add(txtlogin);
+            Controls.Add(button1);
+            Name = "TelaLogin";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox txtlogin;
+        private TextBox txtPassword;
+        private Label label1;
+        private Label label2;
+    }
+}
