9389f4430ea0f67076dd04ddc6718c406fb5fc85
Thor Morciani <gtac.tomorciani@voegol.com.br>
2025-11-25 20:34:31 -0300

    inserindo perfil no token

--- Backend/AIssist/AIssist.Application.Api/Services/AuthService.cs
+++ Backend/AIssist/AIssist.Application.Api/Services/AuthService.cs
@@ -80,12 +80,14 @@ namespace AIssist.Application.Api.Services
 
         private string CreateToken(Users user)
         {
+            var profile = _profileService.GetById(user.ProfileId).Result;
             var claims = new List<Claim>
             {
                 new Claim(ClaimTypes.Name, user.Username),
                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),               
                 new Claim(ClaimTypes.GivenName, user.Name),
-                new Claim(ClaimTypes.Email, user.Email)
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, profile.ProfileName)
             };
 
             var key = new SymmetricSecurityKey(
