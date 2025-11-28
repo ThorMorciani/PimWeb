4ecd11ebd984435ca1ab84d8c93853edf2d93d01
MuriloCS <Murilocs.sjc@gmail.com>
2025-11-27 21:15:40 -0300

    Feat: Responsividade

--- Frontend/AIssist/src/app/layouts/dashboard-layout/dashboard-layout.component.ts
+++ Frontend/AIssist/src/app/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { MatIcon } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [RouterModule, SidebarComponent],
+  imports: [RouterModule, SidebarComponent, MatIcon],
   templateUrl: './dashboard-layout.component.html',
   styleUrls: ['./dashboard-layout.component.scss']
 })
-export class DashboardLayoutComponent {}
+export class DashboardLayoutComponent {
+  mobileOpen = false;
+
+}
